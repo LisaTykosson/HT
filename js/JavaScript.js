ff9a80c1a0524e152b989fefe7260c0420aed63a
LisaTykosson <Lisa.tykosson@vll.se>
2016-12-06 08:44:19 +0100

    Add files via upload

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -53,3 +53,84 @@ function konvmening() {
         document.getElementById("konvexitet").style.display = "none";
     }
 }
+function infektiosa() {
+    if (document.getElementById("infektion").checked)
+        document.getElementById("infekt").style.display = "block";
+    else {
+        document.getElementById("infekt").style.display = "none";
+    }
+}
+function postop() {
+    if
+        (document.getElementById("postopkomp").selectedIndex == "2") {
+        document.getElementById("komplikationer").style.display = "block";
+    }
+    else {
+        document.getElementById("komplikationer").style.display = "none";
+    }
+}
+function kardiovask() {
+    if (document.getElementById("kardiov").checked)
+        document.getElementById("kardio").style.display = "block";
+    else {
+        document.getElementById("kardio").style.display = "none";
+    }
+}
+function neurokirurg() {
+    if (document.getElementById("neurokir").checked)
+        document.getElementById("neuro").style.display = "block";
+    else {
+        document.getElementById("neuro").style.display = "none";
+    }
+}
+function clavpneu() {
+    if (document.getElementById("pneu").checked)
+        document.getElementById("clav").style.display = "block";
+    else {
+        document.getElementById("clav").style.display = "none";
+    }
+}
+function lokalisationkir() {
+    if
+                        (document.getElementById("lokalkir").selectedIndex == "1") {
+        document.getElementById("meningiomkir").style.display = "block"
+        document.getElementById("ovrigakir").style.display = "none";
+        document.getElementById("ryggarkir").style.display = "none";
+    }
+    else if (
+        document.getElementById("lokalkir").selectedIndex == "2") {
+        document.getElementById("ovrigakir").style.display = "block"
+        document.getElementById("meningiomkir").style.display = "none";
+        document.getElementById("ryggarkir").style.display = "none";
+    }
+    else if (
+    document.getElementById("lokalkir").selectedIndex == "3") {
+        document.getElementById("ryggarkir").style.display = "block"
+        document.getElementById("meningiomkir").style.display = "none";
+        document.getElementById("ovrigakir").style.display = "none";
+    }
+
+    else {
+        document.getElementById("meningiomkir").style.display = "none"
+        document.getElementById("ryggarkir").style.display = "none";
+        document.getElementById("ovrigakir").style.display = "none";
+    }
+}
+function reg() {
+    if 
+        (document.getElementById("regr").selectedIndex == "2") {
+        document.getElementById("regress").style.display = "block";
+    }
+    else {
+        document.getElementById("regress").style.display = "none";
+    }
+}
+function nytill() {
+    if 
+        (document.getElementById("nya").selectedIndex == "2") {
+        document.getElementById("bortfall").style.display = "block";
+    }
+    else {
+        document.getElementById("bortfall").style.display = "none";
+    }
+}
